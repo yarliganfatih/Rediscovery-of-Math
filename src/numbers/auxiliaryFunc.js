9757dbcbc1b4bd05cc6c6ad2c7af7e0aae894465
Fatih <fatih.yarligan@bahcesehir.edu.tr>
2023-07-05 13:45:31 +0300

    manual invertByMult function

--- src/numbers/auxiliaryFunc.js
+++ src/numbers/auxiliaryFunc.js
@@ -4,7 +4,18 @@ const isNegative = (a) => a<0;
 
 const invertByAdd = (a) => -a;
 
-const invertByMult = (a) => 1/a;
+const invertByMult = (a) => { // 1/a
+    if(!(a>=-1 && a<=1)){
+        return `1 / ${a}`; // decimal
+    }
+    let denom = 1;
+    while(remain(a, 1) || remain(denom, a)){
+        denom = mult(denom, 10);
+        a = mult(a, 10);
+    }
+    return div(denom, a);
+}
+// TODO turn(0.10) does not work
 
 const abs = (a) => isNegative(a) ? -a : a;
 
